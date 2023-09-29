29ec0cb03362ef5511e3a3b1e68d4fd111040361
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-09-29 15:04:04 +0100

    get tulips from repository

--- src/main/java/mikamarisol/acnh/hybrid/entity/Tulip.java
+++ src/main/java/mikamarisol/acnh/hybrid/entity/Tulip.java
@@ -1,19 +1,22 @@
 package mikamarisol.acnh.hybrid.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 // entity -> single persistent instance of a domain object
 // model -> domain object representing real world object
 @Entity
-//@Table(name = "tulips")
+@Getter
+@Setter
 public class Tulip {
 
     @Id
     private String genotype;
     private String colour;
 
-    // JPA required no-arg constructor
+    // JPA requires no-arg constructor
     public Tulip() {
     }
 
