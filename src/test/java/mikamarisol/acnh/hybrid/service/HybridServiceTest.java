1ab5f0799073b79d63b261203c2777c45f87f11f
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-09-29 22:47:49 +0100

    springboot test

--- src/test/java/mikamarisol/acnh/hybrid/service/HybridServiceTest.java
+++ src/test/java/mikamarisol/acnh/hybrid/service/HybridServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 public class HybridServiceTest {
 
     @Autowired
