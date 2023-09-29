5735185f821b8f598fb65c44d9f0ab8de950e154
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-09-29 16:31:03 +0100

    breed single trait successfully

--- src/main/java/mikamarisol/acnh/hybrid/service/FlowerBreedingService.java
+++ src/main/java/mikamarisol/acnh/hybrid/service/FlowerBreedingService.java
@@ -1,11 +1,23 @@
 package mikamarisol.acnh.hybrid.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FlowerBreedingService {
 
     public List<String> getChildren(String genotypeOne, String genotpyeTwo) {
-        return Arrays.asList("RR", "RR", "Rr", "Rr");
+        List<String> children = new ArrayList<>();
+        for (int i = 0; i < genotypeOne.length(); i++) {
+            for (int j =0; j < genotpyeTwo.length(); j++) {
+                List<String> child = Arrays.asList(String.valueOf(genotypeOne.charAt(i)),
+                        String.valueOf(genotpyeTwo.charAt(j)));
+                Collections.sort(child);
+                children.add(child.stream().map(Object::toString).collect(Collectors.joining()));
+            }
+        }
+        return children;
     }
 }
