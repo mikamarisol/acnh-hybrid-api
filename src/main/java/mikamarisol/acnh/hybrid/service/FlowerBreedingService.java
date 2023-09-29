99c6b348c1de5c6addaf0630c488a71f2dd3985c
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-09-29 15:53:13 +0100

    starting breeding service

---
+++ src/main/java/mikamarisol/acnh/hybrid/service/FlowerBreedingService.java
@@ -0,0 +1,11 @@
+package mikamarisol.acnh.hybrid.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FlowerBreedingService {
+
+    public List<String> getChildren(String genotypeOne, String genotpyeTwo) {
+        return Arrays.asList("RR", "RR", "Rr", "Rr");
+    }
+}
---
+++ src/test/java/mikamarisol/acnh/hybrid/service/FlowerBreedingServiceTest.java
@@ -0,0 +1,37 @@
+package mikamarisol.acnh.hybrid.service;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FlowerBreedingServiceTest {
+
+    private FlowerBreedingService flowerBreedingService = new FlowerBreedingService();
+    @ParameterizedTest
+    @ArgumentsSource(FlowerBreedingArgumentsProvider.class)
+    void breedSingleTrait(String parentOne, String parentTwo, List<String> expectedChildren) {
+        List<String> actualChildren = flowerBreedingService.getChildren(parentOne, parentTwo);
+        assertThat(actualChildren).containsExactlyInAnyOrderElementsOf(expectedChildren);
+    }
+
+    static class FlowerBreedingArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of("RR", "RR", Arrays.asList("RR", "RR", "RR", "RR")),
+                    Arguments.of("Rr", "RR", Arrays.asList("RR", "RR", "Rr", "Rr")),
+                    Arguments.of("rr", "RR", Arrays.asList("Rr", "Rr", "Rr", "Rr")),
+                    Arguments.of("rr", "Rr", Arrays.asList("Rr", "Rr", "rr", "rr")),
+                    Arguments.of("rr", "rr", Arrays.asList("rr", "rr", "rr", "rr"))
+            );
+        }
+    }
+}
