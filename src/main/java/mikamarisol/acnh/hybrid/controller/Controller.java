87a739b4aa675ecdd37b9d926b6697f392e3e15e
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-09-29 16:51:54 +0100

    add breed tulips endpoint

--- src/main/java/mikamarisol/acnh/hybrid/controller/Controller.java
+++ src/main/java/mikamarisol/acnh/hybrid/controller/Controller.java
@@ -1,7 +1,10 @@
 package mikamarisol.acnh.hybrid.controller;
 import mikamarisol.acnh.hybrid.entity.Tulip;
 import mikamarisol.acnh.hybrid.repository.TulipRepository;
+import mikamarisol.acnh.hybrid.service.FlowerBreedingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -9,14 +12,17 @@ import java.util.List;
 public class Controller {
 
     private final TulipRepository tulipRepository;
+    private final FlowerBreedingService flowerBreedingService;
 
-    public Controller(TulipRepository tulipRepository) {
+    @Autowired
+    public Controller(TulipRepository tulipRepository, FlowerBreedingService flowerBreedingService) {
         this.tulipRepository = tulipRepository;
+        this.flowerBreedingService = flowerBreedingService;
 
         this.tulipRepository.saveAll(List.of(
-                new Tulip("rryyss", "White"),
-                new Tulip("rryySs", "White"),
-                new Tulip("rryySS", "White")
+                new Tulip("RR", "Red"),
+                new Tulip("Rr", "Red"),
+                new Tulip("rr", "White")
         ));
     }
 
@@ -24,4 +30,11 @@ public class Controller {
     Iterable<Tulip> getTulips() {
         return tulipRepository.findAll();
     }
+
+    @GetMapping("/breed-tulips/{genotypeOne}&{genotypeTwo}")
+    Iterable<Tulip> breedTulips(@PathVariable String genotypeOne, @PathVariable String genotypeTwo) {
+        List<String> children = flowerBreedingService.getChildren(genotypeOne, genotypeTwo);
+        return tulipRepository.findAllById(children);
+    }
+
 }
--- src/main/java/mikamarisol/acnh/hybrid/service/FlowerBreedingService.java
+++ src/main/java/mikamarisol/acnh/hybrid/service/FlowerBreedingService.java
@@ -1,17 +1,20 @@
 package mikamarisol.acnh.hybrid.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class FlowerBreedingService {
 
     public List<String> getChildren(String genotypeOne, String genotpyeTwo) {
         List<String> children = new ArrayList<>();
         for (int i = 0; i < genotypeOne.length(); i++) {
-            for (int j =0; j < genotpyeTwo.length(); j++) {
+            for (int j = 0; j < genotpyeTwo.length(); j++) {
                 List<String> child = Arrays.asList(String.valueOf(genotypeOne.charAt(i)),
                         String.valueOf(genotpyeTwo.charAt(j)));
                 Collections.sort(child);
