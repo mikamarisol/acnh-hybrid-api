c33e20ec8edb5925906cfd5d0a15523cd7efed65
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-09-29 22:46:34 +0100

    toString methods for records

--- src/main/java/mikamarisol/acnh/hybrid/model/Gene.java
+++ src/main/java/mikamarisol/acnh/hybrid/model/Gene.java
@@ -2,4 +2,8 @@ package mikamarisol.acnh.hybrid.model;
 
 public record Gene(String alleleOne, String alleleTwo) {
 
+    @Override
+    public String toString() {
+        return alleleOne + alleleTwo;
+    }
 }
--- src/main/java/mikamarisol/acnh/hybrid/model/Genotype.java
+++ src/main/java/mikamarisol/acnh/hybrid/model/Genotype.java
@@ -1,6 +1,14 @@
 package mikamarisol.acnh.hybrid.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record Genotype(List<Gene> genes) {
+
+    @Override
+    public String toString() {
+        return genes.stream()
+                .map(Record::toString)
+                .collect(Collectors.joining("-"));
+    }
 }
