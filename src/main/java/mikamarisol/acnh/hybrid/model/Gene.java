eddec31455626cfabedcac790b732a463ba06ed8
mikamarisol <57419248+mikamarisol@users.noreply.github.com>
2023-10-05 11:08:50 +0100

    refactor: null handling + immutable list

--- src/main/java/mikamarisol/acnh/hybrid/model/Gene.java
+++ src/main/java/mikamarisol/acnh/hybrid/model/Gene.java
@@ -1,7 +1,7 @@
 package mikamarisol.acnh.hybrid.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public record Gene(String alleleOne, String alleleTwo) {
@@ -9,14 +9,14 @@ public record Gene(String alleleOne, String alleleTwo) {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Gene)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         return this.toString().equals(o.toString());
     }
 
     @Override
     public String toString() {
-        List<String> alleles = new ArrayList<>(List.of(alleleOne, alleleTwo));
-        Collections.sort(alleles);
+        List<String> alleles = Arrays.asList(alleleOne, alleleTwo);
+        alleles.sort(Comparator.naturalOrder());
         return String.join("", alleles);
     }
 }
